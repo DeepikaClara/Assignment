58fd7044188226f68c46c199a4f5cc37b5e77e04
DEEPIKA <97511874+DeepikaClara@users.noreply.github.com>
2022-01-13 13:39:56 +0530

    Add files via upload

---
+++ Assignment 2 (11-01-2022)/Int &Double Addition/Add_int&double.java
@@ -0,0 +1,9 @@
+class Addition2
+{
+public static void main(String args[])
+{
+int a=5;
+double b=6.2d;
+System.out.println("Addition="+a+b);
+}
+}
---
+++ Assignment 2 (11-01-2022)/Rectangle/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle
+{
+public static void main(String args[])
+{
+int l=5,b=7,A,P;
+A=l*b;
+P=2*(l+b);
+System.out.println("Area of rectangle="+A);
+System.out.println("Perimeter of rectangle="+P);
+}
+}
+
