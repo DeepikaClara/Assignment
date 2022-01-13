7326a99d10c4398335e362047fd80dd7b6eb9f81
DEEPIKA <97511874+DeepikaClara@users.noreply.github.com>
2022-01-13 13:38:08 +0530

    Add files via upload

---
+++ Assignment1(7-01-2022)/Addition/Addition.java
@@ -0,0 +1,9 @@
+class Addition
+{
+public static void main(String args[])
+{
+int a=5, b=8, c;
+c= a+b;
+System.out.println("Addition="+c);
+}
+}
---
+++ Assignment1(7-01-2022)/Division/Division.java
@@ -0,0 +1,10 @@
+class Division
+{
+public static void main(String args[])
+{
+int a=1000, b=10;
+int c;
+c=a/b;
+System.out.println("Division = "+c);
+}
+}
