96d1a3a4d27e91272845797475e391ebbd1a500a
DEEPIKA <97511874+DeepikaClara@users.noreply.github.com>
2022-01-19 20:51:12 +0530

    Add files via upload

---
+++ Assignment 4 (17-01-2022)/Comparing 2 nos/Comparison.java
@@ -0,0 +1,8 @@
+class Comparison
+{
+public static void main(String args[])
+{
+int a=7,b=12;
+System.out.println((a<b)?a:b);
+}
+}
---
+++ Assignment 4 (17-01-2022)/Comparing signed & unsigned/Comparison_signed_unsigned.java
@@ -0,0 +1,8 @@
+class Comparison_signed_unsigned
+{
+public static void main(String args[])
+{
+int a=21,b=-21;
+System.out.println(a<b);
+}
+}
